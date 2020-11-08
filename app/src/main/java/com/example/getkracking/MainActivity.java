68b3a1d8631a5577291ae59f513f949abf2dfc82
Rodriguez Manuel Joaquin <manurodriguez@itba.edu.ar>
2020-11-08 19:47:16 -0300

    Agregada splash screen

--- app/src/main/java/com/example/getkracking/MainActivity.java
+++ app/src/main/java/com/example/getkracking/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.getkracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        new Handler().postDelayed(() -> {
+            Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(homeIntent);
+            finish();
+        }, SPLASH_TIME_OUT);
     }
 }
