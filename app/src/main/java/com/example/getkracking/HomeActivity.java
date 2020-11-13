17763365bf6a963b4d05a007b99d35bbf7443e54
Rodriguez Manuel Joaquin <manurodriguez@itba.edu.ar>
2020-11-13 00:54:11 -0300

    Logica pasada a navigation
    Falta arreglar el stack de iconos y ver de solucionar el error que esta ahi comentado

--- app/src/main/java/com/example/getkracking/HomeActivity.java
+++ app/src/main/java/com/example/getkracking/HomeActivity.java
@@ -3,37 +3,45 @@ package com.example.getkracking;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
-
-import com.example.getkracking.fragments.HomeFragment;
-import com.example.getkracking.fragments.PerfilFragment;
-import com.example.getkracking.fragments.SearchFragment;
-import com.example.getkracking.fragments.SocialFragment;
-import com.example.getkracking.fragments.StatsFragment;
+import android.widget.Toast;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.Stack;
+
+import static java.sql.Types.NULL;
 
 public class HomeActivity extends AppCompatActivity {
     BottomAppBar bottomAppBar;
     BottomNavigationView bottomNavigationView;
     FloatingActionButton homeButton;
-    Fragment homeFragment, socialFragment, searchFragment, statsFragment, perfilFragment;
     Stack<Integer> iconSelectedStack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        getSupportActionBar().hide();
         bottomAppBar = findViewById(R.id.bottomAppBar);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.socialFragment, R.id.searchFragment,
+                R.id.homeFragment, R.id.statsFragment, R.id.perfilFragment)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
         homeButton = findViewById(R.id.fabBottomAppBar);
         homeButton.setBackgroundTintList(AppCompatResources.getColorStateList(this, R.color.gray));
         iconSelectedStack = new Stack<>();
@@ -41,109 +49,79 @@ public class HomeActivity extends AppCompatActivity {
         bottomNavigationView.setBackground(null);   //saca la sombra que se previsualiza en la bottom bar
         bottomNavigationView.getMenu().getItem(2).setEnabled(false);    //desactiva el boton placeholder del medio que ayuda en la alineacion
 
-        homeFragment = new HomeFragment();
-        socialFragment = new SocialFragment();
-        searchFragment = new SearchFragment();
-        statsFragment = new StatsFragment();
-        perfilFragment = new PerfilFragment();
-
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             switchFabHomeColor(false); //por si estaba prendido
+
             // no uso switch por un tema de retrocompatibilidad
             if (item.getItemId() == R.id.menu_social) {
-                showSelectedFragment(socialFragment, "Social");
+                showSelectedFragment(R.id.socialFragment, 0);
             } else if (item.getItemId() == R.id.menu_search) {
-                showSelectedFragment(searchFragment, "Search");
+                showSelectedFragment(R.id.searchFragment, 1);
             } else if (item.getItemId() == R.id.menu_stats) {
-                showSelectedFragment(statsFragment, "Stats");
+                showSelectedFragment(R.id.statsFragment, 3);
             } else if (item.getItemId() == R.id.menu_perfil) {
-                showSelectedFragment(perfilFragment, "Perfil");
+                showSelectedFragment(R.id.perfilFragment, 4);
             }
             return true;
         });
 
-        bottomNavigationView.getMenu().getItem(2).setChecked(true);
-        switchFabHomeColor(true);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragmentContainer, new HomeFragment())
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-        transaction.addToBackStack("Home");
-        transaction.commit();
-//        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new HomeFragment())
-//                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit(); // no uso showSelectedFragment por que quiero evitar el addToBackStack
-//        showSelectedFragment(new HomeFragment(), "Home");
-
+        goToHome(findViewById(android.R.id.content).getRootView());
     }
 
     public void goToHome(View view) {
-        showSelectedFragment(homeFragment, "Home");
-        bottomNavigationView.getMenu().getItem(2).setChecked(true);//desactivo todos los otros activando el placeholder
+        showSelectedFragment(R.id.homeFragment, 2);
+        bottomNavigationView.getMenu().getItem(2).setChecked(true); //desactivo todos los otros activando el placeholder
         switchFabHomeColor(true);
     }
 
-    private void showSelectedFragment(Fragment fragment, String name) {
-        if(getSupportFragmentManager().getBackStackEntryAt(getSupportFragmentManager().getBackStackEntryCount() -1).getName().equals(name))
+    private void showSelectedFragment(int id, int newIconIndex) {
+        NavController controller = Navigation.findNavController(this, R.id.nav_host_fragment);
+        if (!Objects.isNull(controller.getCurrentDestination()) && controller.getCurrentDestination().getId() == id)
             return;
-
-        boolean fragmentPopped = getSupportFragmentManager().popBackStackImmediate(name, 0);
-
-        if (!fragmentPopped) {
-            //fragment no en stack, se debe crear
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.fragmentContainer, fragment)
-                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-            transaction.addToBackStack(name);
-            transaction.commit();
-        }
-
-        int newIconIndex;
-        switch (name) {
-            case "Social":
-                newIconIndex = 0;
-                break;
-            case "Search":
-                newIconIndex = 1;
-                break;
-            case "Stats":
-                newIconIndex = 3;
-                break;
-            case "Perfil":
-                newIconIndex = 4;
-                break;
-            default:
-                newIconIndex = 2;
-        }
-        //no es el mismo que se encontraba prendido antes, saco si se encontraba antes y lo pusheo
-        if (iconSelectedStack.contains(newIconIndex)) {
-            iconSelectedStack.remove((Object) newIconIndex);
-        }
 
         //Busco cual es el icono que estaba prendido antes del cambio para ver si es necesario guardarlo
         for (int i = 0; i < bottomNavigationView.getMenu().size(); i++) {
             if (bottomNavigationView.getMenu().getItem(i).isChecked()) {
-                if (i != newIconIndex) {
-                    iconSelectedStack.push(i);
-                }
+                iconSelectedStack.push(i);
                 break;
             }
         }
+        boolean poppedFragment = controller.popBackStack(id, true);
+
+        if (!poppedFragment) {
+            controller.navigate(id);    //lo creo por que no existe
+        } else {
+            int i = 0;
+            for (; i < iconSelectedStack.size(); i++)
+                if (iconSelectedStack.get(i) == newIconIndex)
+                    break;
+
+            iconSelectedStack.subList(i, iconSelectedStack.size()).clear();
+            Toast.makeText(getBaseContext(),iconSelectedStack.toString(),Toast.LENGTH_SHORT).show();
+        }
+
+            /*
+            VER SI SE PUEDE ARREGLAR LA SITUACION EN LA QUE TENGO
+            A-> B-> C-> D
+            voy a A y me queda
+            A
+            estaria bueno que quede B-> C-> D-> A
+             */
+
     }
+
 
     @Override
     public void onBackPressed() {
-        if (getSupportFragmentManager().getBackStackEntryCount() == 1)
-            finish();
-        else {
-            super.onBackPressed();
+        super.onBackPressed();
 
-            //agrego este codigo para que la bottomNavBar mantenga su comportamiento
-            if (!iconSelectedStack.isEmpty()) {
-                //Toast.makeText(getBaseContext(), String.valueOf(iconSelectedStack.size()), Toast.LENGTH_SHORT).show();
-                int selectedIcon = iconSelectedStack.pop();
-                bottomNavigationView.getMenu().getItem(selectedIcon).setChecked(true);
-                switchFabHomeColor(selectedIcon == 2);
-            }
+        //agrego este codigo para que la bottomNavBar mantenga su comportamiento
+        if (!iconSelectedStack.isEmpty()) {
+            int selectedIcon = iconSelectedStack.pop();
+            bottomNavigationView.getMenu().getItem(selectedIcon).setChecked(true);
+            switchFabHomeColor(selectedIcon == 2);
         }
+
     }
 
     private void switchFabHomeColor(boolean flag) {
