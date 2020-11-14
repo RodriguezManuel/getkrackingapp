dfcd12045d1334bd7dc27b90585e735d0f011250
Ana Cruz <acruz@itba.edu.ar>
2020-11-14 16:55:23 -0300

    tratando de hacer el recycle, a medias

--- app/src/main/java/com/example/getkracking/fragments/StatsFragment.java
+++ app/src/main/java/com/example/getkracking/fragments/StatsFragment.java
@@ -3,6 +3,8 @@ package com.example.getkracking.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,8 +12,16 @@ import android.view.ViewGroup;
 
 import com.example.getkracking.HomeActivity;
 import com.example.getkracking.R;
+import com.example.getkracking.adapters.ExcercisesAdapter;
+import com.example.getkracking.entities.ExcerciseVO;
+import com.example.getkracking.entities.RoutineVO;
+
+import java.util.ArrayList;
 
 public class StatsFragment extends Fragment {
+
+    RecyclerView recyclerExcercise;
+    ArrayList<ExcerciseVO> excercisesList;
 
     public StatsFragment() {
         // Required empty public constructor
@@ -25,9 +35,26 @@ public class StatsFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    private void fillList(){
+        excercisesList.add(new ExcerciseVO("Sentadillas", "40 repeticiones"));
+        excercisesList.add(new ExcerciseVO("Abominables", "50 repeticiones"));
+        excercisesList.add(new ExcerciseVO("Los de culo", "20 minutos"));
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_stats, container, false);
+
+        View vista = inflater.inflate(R.layout.fragment_stats, container, false);
+        excercisesList = new ArrayList<>();
+        recyclerExcercise = vista.findViewById(R.id.RoutineRecycler);
+        recyclerExcercise.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        fillList();
+
+        ExcercisesAdapter adapter = new ExcercisesAdapter(excercisesList);
+        recyclerExcercise.setAdapter(adapter);
+        recyclerExcercise.setNestedScrollingEnabled(false);
+        return vista;
     }
 }
